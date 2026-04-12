95047afce18ec15f2588f3a89285d2176be75c96
Artur M <arturmosevic31@gmail.com>
2026-04-12 15:57:35 +0300

    Добавлена Тема 19

---
+++ Tema19/ExpenseTracker/Controllers/ExpensesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace ExpenseTracker.Controllers;
+
+public class ExpensesController : Controller
+{
+    private static List<Expense> expenses = new List<Expense>();
+
+    public IActionResult Index()
+    {
+        return View(expenses);
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(string Amount, string Category)
+    {
+        Amount = Amount.Replace(",", ".");
+
+        decimal amount = decimal.Parse(Amount, CultureInfo.InvariantCulture);
+
+        var expense = new Expense
+        {
+            Id = expenses.Count + 1,
+            Amount = amount,
+            Category = Category,
+            Date = DateTime.Now
+        };
+
+        expenses.Add(expense);
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Filter(string category)
+    {
+        var filtered = expenses
+            .Where(e => e.Category != null && e.Category.ToLower() == category.ToLower())
+            .ToList();
+
+        return View(filtered);
+    }
+}
